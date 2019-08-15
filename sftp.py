436e221fd0cfcad0ebf8a6659ee66ed032897f52
Ivan Lemos <29108828+IvanLemos5000@users.noreply.github.com>
2019-08-15 12:25:42 -0300

    adjust def mkdir

--- sftp.py
+++ sftp.py
@@ -103,10 +103,10 @@ class Connection(object):
         self._sftp_connect()
         self._sftp.rename(oldpath, newpath)
 
-    def mkdir(self, path, mode=0777):
+    def mkdir(self, path, mode=777):
         """ Create New dir on given path, as given file permission """
         self._sftp_connect()
-        self._sftp.mkdir(path, mode=0777)
+        self._sftp.mkdir(path, mode=777)
         
     def chmod(self, path, mode):
         """ Change the permision of path """
